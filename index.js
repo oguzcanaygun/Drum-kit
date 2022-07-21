8f39a543f0603e4a1b788eae97940d5749d4b4a0
Oguzcan Aygun <aygunoguz51@gmail.com>
2022-07-21 17:01:47 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+document.addEventListener('keydown', function(event) {
+    playInstrument(event.key);
+});
+
+document.querySelectorAll(".drum").forEach(function(btn) {
+    btn.addEventListener("click", handleClick);
+});
+
+
+function handleClick() {
+    var clickedOn = this.innerText;
+    playInstrument(clickedOn);
+}
+
+function playInstrument( key ) {
+    var soundToPlay = "";
+    switch (key) {
+        case 'w':
+            soundToPlay = "sounds/tom-1.mp3";
+            break;
+        case 'a':
+            soundToPlay = "sounds/tom-2.mp3";
+            break;
+        case 's':
+            soundToPlay = "sounds/tom-3.mp3";
+            break;
+        case 'd':
+            soundToPlay = "sounds/tom-4.mp3";
+            break;
+        case 'j':
+            soundToPlay = "sounds/crash.mp3";
+            break;
+        case 'k':
+            soundToPlay = "sounds/kick-bass.mp3";
+            break;
+        case 'l':
+            soundToPlay = "sounds/snare.mp3";
+            break;
+        default:
+            console.log(key);
+    }
+    if (soundToPlay.length > 1) {
+        var audio = new Audio(soundToPlay);
+        audio.play();
+    }
+}
